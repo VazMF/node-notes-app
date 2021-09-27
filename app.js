00e7d0f87d2b3a1858626d3021f89f4566b0d11f
VazMF <vazfernandam@gmail.com>
2021-09-27 09:07:26 -0300

    printando mensagem colorida no terminal

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log(msg)
 
-console.log(validator.isURL('https://github.com/VazMF'))
+console.log(chalk.bold.inverse.green('Success!'))
