9e49025a10399fb2e1b2d695b255d0639db76664
VazMF <vazfernandam@gmail.com>
2021-09-30 11:48:01 -0300

    refatorando as funçoes para usar arrow

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body);
     }
 });
@@ -34,7 +34,7 @@ yargs.command({
             type: 'string'
         },
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title);
     }
 });
@@ -43,7 +43,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function () {
+    handler() {
         console.log('Listing all notes');
     }
 });
@@ -52,7 +52,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
+    handler() {
         console.log('Reading a note')
     }
 });
